3046031dd39d18c724540bac422fe943b4d7a246
nedlrichards <edwardlrichards@gmail.edu>
2019-03-08 10:26:11 -0800

    cleaning up codebase

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='fish_hawk',
+setup(name='zoidberg',
       version='0.1',
       description='Pixhawk interface for zoidberg submarine',
       url='http://github.com/nedlrichards/fish_hawk',
-      author='Edward Richards',
-      author_email='edwardlrichards@gmail.com',
+      author='San Diego city robotics club',
+      author_email='sdcrobotics@gmail.com',
       license='MIT',
-      packages=['fish_hawk'],
+      packages=['zoidberg'],
       install_requires=[
           'pymavlink',
           'pyserial',
--- zoidberg/__init__.py
+++ zoidberg/__init__.py
@@ -3,12 +3,6 @@ from .pixhawk_readings import PixhawkReading
 from .pixhawk_node import PixhawkNode
 
 try:
-    import hid
-    from .logitech_cntrl_node import ManualControlNode
-except ImportError:
-    pass
-
-try:
     import pyzed
     from .zed_node import ZedNode
 except ImportError:
