4b112fdf168d03e5e2b7521df0d0ab3dc18f66e2
nedlrichards <edwardlrichards@gmail.edu>
2019-03-21 15:45:00 -0700

    saving depth images as jpeg

--- zoidberg_cc/zed_out.py
+++ zoidberg_cc/zed_out.py
@@ -1,22 +1,19 @@
-from zoidberg import ZedNode, pause
-from time import time
+from zoidberg import ZedNode, pause, episode
 import cv2
 
-update_period = 0.05
-runzed = ZedNode()
+runnum = episode()
+zn = ZedNode()
 try:
     print("starting up communication with Zed camera")
-    runzed.isactive(True)
-    runzed.print_camera_information()
+    zn.isactive(True)
+    zn.print_camera_information()
     while True:
-        loop_start = time()
-        isnew = runzed.check_readings()
+        isnew = zn.check_readings()
         if isnew:
-            cv2.imshow("image", runzed.image)
-            cv2.imshow("depth", runzed.depth)
-        # sleep to maintain constant rate
-        pause(loop_start, update_period)
+            cv2.imshow("image", zn.image)
+            cv2.imshow("depth", zn.depth)
+            zn.log(runnum)
 finally:
     print("Shutting down communication with Zed camera")
     cv2.destroyAllWindows()
-    runzed.isactive(False)
+    zn.isactive(False)
