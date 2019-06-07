ad7ac60ed86d42ea9897a6be3389d69496de84b9
nedlrichards <edwardlrichards@gmail.edu>
2019-06-07 11:22:16 -0700

    added vision node to initfile

--- zoidberg/__init__.py
+++ zoidberg/__init__.py
@@ -3,3 +3,5 @@ from .main_loop import actionperiod, main_loop
 from .mission_blocks import constant_r_success, constant_r_task
 from .pixhawk_node import PixhawkNode
 from .zed_node import ZedNode
+from .vision_node import VisionNode
+from .detection import Detection
