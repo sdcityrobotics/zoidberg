2dc8170ecf73872e28be3e4a06249ca8142d794c
nedlrichards <edwardlrichards@gmail.edu>
2019-06-12 12:09:33 -0700

    trying to clean up zoidberg mission code

--- zoidberg/__init__.py
+++ zoidberg/__init__.py
@@ -1,6 +1,6 @@
 from .utils import timestamp, pause, empty_value, episode, heading_diff
-from .main_loop import actionperiod, main_loop
-from .mission_blocks import constant_r_success, constant_r_task
+from .mission_blocks import actionperiod, main_loop,\
+                            constant_r_success, constant_r_task
 from .pixhawk_node import PixhawkNode
 
 # vision processing requires open cv, which should be optional install
--- zoidberg_cc/heading_task.py
+++ zoidberg_cc/heading_task.py
@@ -2,8 +2,7 @@ from time import time
 import numpy as np
 import matplotlib.pyplot as plt
 from serial import SerialException
-from zoidberg import PixhawkNode, pause, episode, actionperiod, main_loop, \
-                     constant_r_task, constant_r_success
+from zoidberg import PixhawkNode, episode, change_heading
 
 # Linux address
 device = '/dev/ttyACM0'
@@ -17,9 +16,6 @@ pn = PixhawkNode(device)
 
 total_time = 30  # total task time, seconds
 target_heading = 80  # random choice
-r_P = 3  # proportionality constant for heading correction
-r_max = 70  # max rate of turn
-r_tol = 5  # if we are within this number of degrees, we have succeeded
 
 # create dictionary of nodes needed for this task
 node_dict = {'pn':pn}
@@ -31,11 +27,11 @@ try:
     pn.isactive(True)
     pn.change_mode('MANUAL')  # change to manual mode
 
-    r_task = constant_r_task(target_heading, r_P, r_max)
-    r_succ = constant_r_success(target_heading, r_tol)
-
-    main_loop(node_dict, runnum, total_time, isdone=r_succ, get_r=r_task)
-    print('Reached desired heading')
+    is_timeout = change_heading(node_dict, runnum, total_time, target_heading)
+    if is_timeout:
+        print('Reached desired heading')
+    else:
+        print('Mission timed out')
 
 except SerialException:
     print('Pixhawk is not connected to %s'%device)
